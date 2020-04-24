dd1183c0f51039fa7570a9ceb0fc34ec4e0d77c4
thuy.nguyenxuan.ncc@gmail.com <thuy.nguyenxuan.ncc@gmail.com>
2020-04-24 19:30:28 +0700

    dd

--- Goodbye/Test.cs
+++ Goodbye/Test.cs
@@ -1,11 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Goodbye
 {
-    class Test
+    class Sinhvien
     {
-        public int MyProperty { get; set; }
+        static void Main(string[] args)
+        {
+            Method1();
+            Method2();
+            Console.ReadKey();
+        }
+
+        public static async Task Method1()
+        {
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine(" Method 1");
+                }
+            });
+        }
+
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine(" Method 2");
+            }
+        }
     }
+
 }
